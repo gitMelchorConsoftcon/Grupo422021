fb6f6c2104d409400f84b34ea412ed8a133a5f16
Melchor Gabriel Lopez Castro <melchorgabriel@gmail.com>
2021-09-08 11:20:29 -0600

    Incio de proyecto

---
+++ Grupo422021/Grupo422021.Web1/Controllers/MecanicosController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace Grupo422021.Web1.Controllers
+{
+    public class MecanicosController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            var mecanicos = new List<string>();
+
+            mecanicos.Add("Melchor");
+            mecanicos.Add("Gabriel");
+            mecanicos.Add("Pedro");
+            mecanicos.Add("Juan");
+            mecanicos.Add("Marcos");
+
+            return View(mecanicos);
+        }
+    }
+}
